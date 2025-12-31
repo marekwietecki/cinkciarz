9555ba072035e684511aeae49fc7347b5c65aec3
marekwietecki <marekwietecki10@gmail.com>
2025-12-31 08:40:01 +0100

    xyz

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -1,24 +1,21 @@
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-// Definiujemy kolory bezpośrednio tutaj, zamiast je importować
 const MyColors = {
   light: {
     text: '#11181C',
     background: '#fff',
     tint: '#2f95dc',
-    // ... dopisz swoje kolorki
   },
   dark: {
     text: '#ECEDEE',
     background: '#151718',
     tint: '#fff',
-    // ... dopisz swoje kolorki
   },
 };
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: 'text' | 'background' | 'tint' // wpisz nazwy kluczy których używasz
+  colorName: 'text' | 'background' | 'tint' 
 ) {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
@@ -26,7 +23,6 @@ export function useThemeColor(
   if (colorFromProps) {
     return colorFromProps;
   } else {
-    // @ts-ignore - żeby nie walczyć z typami przez chwilę
     return MyColors[theme][colorName];
   }
 }
